d75df372aa5bd1c8b48f59c0968a2a211b6b3512
Posse <posse666666@gmail.com>
2021-02-17 20:51:07 +0300

    Added second layout and switching between them.

--- app/src/main/java/com/posse/android1/calculator/MainActivity.java
+++ app/src/main/java/com/posse/android1/calculator/MainActivity.java
@@ -2,13 +2,27 @@ package com.posse.android1.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void changeLayout(View view) {
+        setContentView(R.layout.activity_additional);
+    }
+
+    public void changeLayout2(View view) {
         setContentView(R.layout.activity_main);
     }
 }
